1f7cdfb7f689448ed9671565c29a7bd3825bfaea
Giovanni-2025 <giovanni.emil@gmail.com>
2026-05-02 16:27:27 +0300

    Fix Loader color

--- Frontend/src/components/Loader/Loader.jsx
+++ Frontend/src/components/Loader/Loader.jsx
@@ -4,7 +4,7 @@ import styles from "./Loader.module.css";
 export default function Loader() {
   return (
     <Html center>
-      <div className="bg-light vw-100 vh-100 d-flex flex-column justify-content-center align-items-center">
+      <div className="bg-secondary vw-100 vh-100 d-flex flex-column justify-content-center align-items-center">
         <span className={styles.loader}></span>
       </div>
     </Html>
